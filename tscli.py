620b8f3f3c01cc6ff918a31a1f7fb36ffb9cb2f0
Adrian Puente Z <ch0ks@users.noreply.github.com>
2016-05-06 20:00:27 -0700

    Dump policy into MD files
    Now the script can list the policies and dumo them into files with in markup format using a directory structure. This is with the purpose of being able to keep versioning of the policies in github.

--- tscli.py
+++ tscli.py
@@ -5,7 +5,6 @@ __company__ = 'Hackarandas Inc.'
 import os
 import sys
 import json
-import urllib
 import unirest
 import argparse
 import configparser
@@ -42,12 +41,11 @@ def ts_apiclient(key, api_name):
     :return: the infomration retrieved in dictionary format,
     """
     url = 'https://app.threatstack.com/api/v1/' + api_name
-    hders = {'Authorization': key}
-    response = unirest.get(url, headers=hders)
-    response = response.raw_body
-    data = json.loads(response)
+    headers = {'Authorization': key}
+    response = unirest.get(url, headers=headers)
+    response_raw = response.raw_body
+    data = json.loads(response_raw)
     return data
-
 
 def ts_getagents(auth_token):
     """
@@ -96,7 +94,74 @@ def ts_getalerts(auth_token):
     :type auth_token: str
     :return: authentication token.
     """
-    return ts_apiclient(key, 'alerts')
+    data = ts_apiclient(auth_token, 'agents')
+    return data
+
+def ts_policies(auth_token):
+    """
+    This function is a wrapper for the api_client to retrieve the policies. 
+    It will validate that the parameters are the correct ones and request the 
+    information from the client.
+
+    These are the parameters that you can pass to the function.:
+
+    :param auth_token: used to select the section of the config file. By default 'default' is used.
+    :type auth_token: str
+    :return: authentication token.
+    """
+    data = ts_apiclient(auth_token, 'policies')
+    return data
+
+def ts_printpolicies(Policies):
+    """
+    This function retrieves all the policies and prints all the details in a ordely manner.
+
+    These are the parameters that you can pass to the function.:
+
+    :param auth_token: used to select the section of the config file. By default 'default' is used.
+    :type auth_token: str
+    :return: authentication token.
+    """
+    for policy in Policies:
+      printSuccess("Name: %s, Number of Alerts %s" %(policy['name'] , policy['alert_rule_count']))
+      for alert_policy in policy['alert_policy']:
+        #printSuccess("-------------")
+        printSuccess("\tTitle: %s" %alert_policy['title'])
+        for key in alert_policy.keys():
+          if key != 'exclusions' and key != 'title': printSuccess("\t\t%s: %s" %(key,alert_policy[key]))
+        printSuccess("\t\tExclusions:")
+        if alert_policy['exclusions']: printSuccess("\t\t\t%s" %'\n[+]\t\t\t'.join(alert_policy['exclusions']))
+
+def ts_dumppolicies(Policies):
+    """
+    This function retrieves all the policies and creates the directory structure and dumps the policies
+    in markup format so it can be exported to github.
+
+    These are the parameters that you can pass to the function.:
+
+    :param auth_token: used to select the section of the config file. By default 'default' is used.
+    :type auth_token: str
+    :return: authentication token.
+    """
+    for policy in Policies:
+      directory=policy['name'].strip()
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+        printSuccess("Directory %s does not exists, created." %directory)
+      else: printSuccess("Directory %s exists, populating.")
+
+      for alert_policy in policy['alert_policy']:
+        filename=alert_policy['title']+'.md'.strip()
+        mdfile="# %s" %alert_policy['title']
+        mdfile=mdfile+"%s\n\n" %alert_policy['description']
+        mdfile=mdfile+"## Enabled\n\n```\n%s\n```\n\n" %alert_policy['enabled']
+        mdfile=mdfile+"## Severity\n\n```\n%s\n```\n\n" %alert_policy['severity']
+        mdfile=mdfile+"## Filter\n\n```\n%s\n```\n\n" %alert_policy['filter']
+        mdfile=mdfile+"## Supression Rules\n\n```\n%s\n```\n\n" %'\n'.join(alert_policy['exclusions'])
+        printSuccess("\tCreating file policy: %s" %filename)
+        f = open(directory+'/'+filename, 'w')
+        f.write(mdfile)
+        f.close()
 
 def main():
     now = strftime("%Y%m%d-%H%M%S")
@@ -118,6 +183,10 @@ def main():
     parser.add_argument("-l", "--list-logs",
                         action="store_true",
                         help="lists all the agents in the organization")
+    parser.add_argument("-d", "--dump-policies",
+                        action="store_true",
+                        help="dumps all the policies in the organization into individual md files")
+
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
@@ -128,11 +197,12 @@ def main():
     elif args.list_alerts:
         ts_getalerts(ts_readconfig())
     elif args.list_policies:
-        printError("Not yet implemented")
+        ts_printpolicies(ts_policies(ts_readconfig()))
     elif args.list_organizations:
         printError("Not yet implemented")
     elif args.list_logs:
         printError("Not yet implemented")
-
+    elif args.dump_policies:
+        ts_dumppolicies(ts_policies(ts_readconfig()))
 
 if __name__ == "__main__": main()
